17bbd507dc22aeec3c52e0f6c8a775f4426dd575
Nathnael Yimer <natnaelyimer2040@gmail.com>
2025-04-22 09:54:32 +0300

    Revert "Merge pull request #1 from NathnaelYimer/backend"
    This reverts commit 05b692349f5caf84b6f4c08f6a98a96df888cd42, reversing
    changes made to 5bd6fbfefd4a155964f7aca3fa6b722c7e5319d6.
    
    git push origin main

--- menu-backend/models/assignment.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type ProductAssignment struct {
-    Level    string   `bson:"level" json:"level"`
-    Products []string `bson:"products" json:"products"`
-}
--- menu-backend/models/terms_conditions.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type LevelTerms struct {
-    Level   string `bson:"level" json:"level"`
-    Title   string `bson:"title" json:"title"`
-    Content string `bson:"content" json:"content"` // HTML string
-}
